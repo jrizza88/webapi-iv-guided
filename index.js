6149d7ceac515358c1df033e0dc3203b09760432
jrizza88 <jetorres88@gmail.com>
2019-03-06 22:09:55 -0500

    added files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config(); // add this line as the first thing to run1
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
